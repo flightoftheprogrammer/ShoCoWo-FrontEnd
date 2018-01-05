deb62b323c26bc736d4afe0b3a62078eb918d94e
Gavin Williams <gjosephw24@gmail.com>
2018-01-05 10:22:57 -0500

    Addded login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,9 +14,11 @@ import {
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RegistrationComponent } from './components/registration/registration.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes = [
   { path: 'register', component: RegistrationComponent },
+  { path: 'login', component: LoginComponent },
   //{ path: '**', component: RegistrationComponent }
 ]
 
@@ -24,7 +26,8 @@ const routes = [
   declarations: [
     AppComponent,
     HeaderComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
