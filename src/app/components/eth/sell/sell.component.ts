1795eb6e47be39fa51c7f64116572192087f9e56
Gavin Williams <gjosephw24@gmail.com>
2018-01-09 15:16:09 -0500

    added the sell pages to btc and eth

--- src/app/components/eth/sell/sell.component.ts
+++ src/app/components/eth/sell/sell.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CryptoService } from '../../../services/crypto.service';
+
 
 @Component({
   selector: 'app-sell',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellEthComponent implements OnInit {
 
-  constructor() { }
+  objectKeys = Object.keys;
+  cryptos: any;
+
+  constructor (private _data: CryptoService) {
+
+  }
 
   ngOnInit() {
+    this._data.getEthPrice()
+    .subscribe(res => {
+      this.cryptos = res;
+      console.log(res);
+    })
   }
 
 }
