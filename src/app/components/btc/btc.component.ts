13662a0905a91399d096055a78d60c9ff9c18fab
Josh Lohman <jjosh9708@gmail.com>
2018-01-12 14:45:10 -0500

    All charts are posted in there page

--- src/app/components/btc/btc.component.ts
+++ src/app/components/btc/btc.component.ts
@@ -9,14 +9,59 @@ import { Chart } from 'chart.js';
 })
 export class BtcComponent implements OnInit {
 
+  chart = [];
+
   constructor(private _chart: ChartService) {}
 
   ngOnInit() {
     this._chart.dailyBtcPrice()
-       .subscribe(res => {
-         console.log(res)
- 
-         let temp_max = res['list'].map(res => res.main)
-       })
-   }
+      .subscribe(res => {
+        this.chart = [];
+
+        Object.keys(res['bpi']).forEach( eachDate => {
+          this.chart.push({
+            date: eachDate,
+            value: res['bpi'][eachDate]
+          });
+        });
+
+        let priceArray =  []
+          this.chart.forEach(x => {
+            priceArray.push(x['value'])
+          });
+        
+        let dateArray = []
+          this.chart.forEach(x => {
+            dateArray.push(x['date'])
+          });
+        
+        this.chart = new Chart( 'canvas' , {
+          type: 'line',
+          data: {
+            labels: dateArray,
+            datasets:[
+              {
+                data: priceArray,
+                borderColor:'#3cba9f',
+                fill: true
+              }
+            ]
+            },
+            options: {
+              legend:{
+                display: false
+              },
+              scales:{
+                xAxes:[{
+                  display: true
+                }],
+                yAxes:[{
+                  display: true
+                }]
+              }
+            }
+          });
+      });
+    
+  }
 }
