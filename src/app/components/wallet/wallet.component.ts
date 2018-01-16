eca643311d48c3d41a2e5e05dc39bc3ad2527aa6
Gavin Williams <gjosephw24@gmail.com>
2018-01-16 16:26:07 -0500

    added connection to api for currency and wallet and wallet transactions

--- src/app/components/wallet/wallet.component.ts
+++ src/app/components/wallet/wallet.component.ts
@@ -1,15 +1,75 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../../services/backend.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CurrencyService } from '../../services/currency.service';
+
 
 @Component({
   selector: 'app-wallet',
+  providers: [BackendService, CurrencyService],
   templateUrl: './wallet.component.html',
   styleUrls: ['./wallet.component.css']
 })
 export class WalletComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _backend: BackendService, private _currency: CurrencyService) { }
 
   ngOnInit() {
   }
 
+  getWallet() {
+    this._backend.getWallet().subscribe(
+      value => {
+        console.log('We got an value from the backend!');
+        console.log('That value is:');
+        console.log(value);
+      },
+      err => { console.log('Error in subscription:'); console.log(err); },
+      () => console.log('Subscription completed!') 
+    )
+  }
+
+  addNumber() {
+    this._backend.updateWalletBalance(3.5).subscribe(value => console.log(value))
+  }
+
+  getWalletTransactions(){
+    this._backend.getWalletTransaction().subscribe(
+      value => {
+        console.log('We got an value from the backend!');
+        console.log('That value is:');
+        console.log(value);
+      },
+      err => { console.log('Error in subscription:'); console.log(err); },
+      () => console.log('Subscription completed!')
+    )
+  }
+
+  addWalletTransaction(amount: number) {
+    this._backend.postWalletTransaction(amount).subscribe(value => console.log(value))
+  }
+
+  getWalletTransaction(id: number) {
+    this._backend.getWalletTransactionById(id).subscribe(value => console.log(value))
+  }
+
+  postCurrency(currencyNameLong: string, currencyNameShort: string) {
+    this._currency.addCurrency(currencyNameLong, currencyNameShort).subscribe(value => console.log(value))
+  }
+
+  getCurrencies(){
+    this._currency.getCurrencies().subscribe(
+      value => {
+        console.log('We got an value from the backend!');
+        console.log('That value is:');
+        console.log(value);
+      },
+      err => { console.log('Error in subscription:'); console.log(err); },
+      () => console.log('Subscription completed!')
+    )
+  }
+
+  getCurrency(id: number) {
+    this._currency.getCurrencyId(id).subscribe(value => console.log(value))
+  }  
 }
--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -1,2 +1,44 @@
 import { Injectable } from '@angular/core';
-import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from '../services/auth.service'
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class BackendService {
+
+  constructor(private _http: HttpClient, private _router: Router, private _auth: AuthService) { }
+
+  getWallet(): Observable<Object> {
+    if (!localStorage.getItem('id_token')) { return new Observable(observer => observer.next(false)); }
+
+    return this._http.get(`http://localhost:53293/api/Wallet`, {headers: this.setHeader()})
+  }
+
+  updateWalletBalance(amountChanged: number): Observable<Object> {
+    return this._http.put(`http://localhost:53293/api/Wallet?amount=${amountChanged}`, null, {headers: this.setHeader()})
+  }
+
+  private setHeader(): HttpHeaders {
+    return new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('id_token')}`);
+  }
+
+  getWalletTransaction(): Observable<Object> {
+    if (!localStorage.getItem('id_token')) { return new Observable(observer => observer.next(false)); }
+
+    return this._http.get(`http://localhost:53293/api/WalletTransaction`, {headers: this.setHeader()})
+  }
+
+  getWalletTransactionById(id: number): Observable<Object> {
+    if (!localStorage.getItem('id_token')) { return new Observable(observer => observer.next(false)); }
+
+    return this._http.get(`http://localhost:53293/api/WalletTransaction/${id}`, {headers: this.setHeader()})
+  }
+
+  postWalletTransaction(amountChanged: number): Observable<Object> {
+    if (!localStorage.getItem('id_token')) { return new Observable(observer => observer.next(false)); }
+
+    return this._http.post(`http://localhost:53293/api/WalletTransaction`,{"TransactionAmount": amountChanged}, {headers: this.setHeader()})
+  }
+
+}
