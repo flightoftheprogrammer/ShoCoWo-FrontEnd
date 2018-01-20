1ebd07efdb23aeba13d48d6e0eb15aa6e12a2971
Jeremy Warren <jeremyawarren15@gmail.com>
2018-01-20 03:00:01 -0500

    corrected backend api calls to btc page

--- src/app/components/eth/eth.component.ts
+++ src/app/components/eth/eth.component.ts
@@ -39,8 +39,8 @@ export class EthComponent implements OnInit {
     this._crypto.getEthPrice().subscribe(result => this.currencyPrice = result["ETH"]["USD"])
     this._backend.getWallet().subscribe(value => this.availableFunds = value['WalletBalance'])
     this._holding.getHoldingByCurrencyId(2).subscribe(result => {
-      this._holding.getHolding(result[1]["HoldingId"]).subscribe(value => {
-        this.ethereumTotal = value[1]["CryptoHoldingBalance"]
+      this._holding.getHolding(result["HoldingId"]).subscribe(value => {
+        this.ethereumTotal = value["CryptoHoldingBalance"]
         this.totalValue = this.currencyPrice * this.ethereumTotal
       })
       this._holding.getHoldingTransactions(result[1]["HoldingId"]).subscribe((wt: WalletTransaction[]) => {
