13b5ece8b8a3aea4630b14ee7824685134ef552c
Jeremy Warren <jeremyawarren15@gmail.com>
2018-01-19 21:04:07 -0500

    changed api urls to match new backend

--- src/app/services/wallet.service.ts
+++ src/app/services/wallet.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-const Api_Url = 'http://shocowo.apphb.com'
+const Api_Url = 'http://shocowo.azurewebsites.net'
 
 @Injectable()
 export class WalletService {
